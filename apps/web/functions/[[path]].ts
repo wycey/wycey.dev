0c566d985f4f946a8bdadffb80b4f0f057a8c03b
instructr13 <instructr13@gmail.com>
2022-12-17 17:19:12 +0900

    chore: customize cloudflare adaptors
    Signed-off-by: instructr13 <instructr13@gmail.com>

--- apps/web/functions/[[path]].ts
+++
@@ -1,4 +0,0 @@
-// Cloudflare Pages Functions
-// https://developers.cloudflare.com/pages/platform/functions/
-// @ts-expect-error imports
-export { onRequest } from "../../../server/entry.cloudflare-pages";
