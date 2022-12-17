057d537582a2c1d8e0ae66fb66859ec15f6e8a7f
instructr13 <instructr13@gmail.com>
2022-12-17 16:40:03 +0900

    Initial commit
    Signed-off-by: instructr13 <instructr13@gmail.com>

---
+++ apps/web/src/components/header/index.tsx
@@ -0,0 +1,50 @@
+import { component$, useStyles$ } from "@builder.io/qwik";
+
+import { Badge } from "@/components/badge";
+
+import NavigationLink from "./elements/navigation-link";
+import _styles, { HeaderBase, Logo, NavigationArea, NavigationList } from "./header.css";
+
+export default component$(() => {
+    useStyles$(_styles);
+
+    return (
+        <HeaderBase>
+            <Logo>
+                <a href="/">わいしぃ</a>
+            </Logo>
+            <NavigationArea>
+                <NavigationList>
+                    <li>
+                        <NavigationLink href="/about">About</NavigationLink>
+                    </li>
+                </NavigationList>
+                <div />
+                <NavigationList>
+                    <li>
+                        <Badge
+                            accent="indigo"
+                            href="https://mido.wycey.dev"
+                            aria-label="Go to the website by Mido"
+                            target="_blank"
+                        >
+                            <Badge.Icon src="/icons/mido.png" alt="Mido" />
+                            Mido
+                        </Badge>
+                    </li>
+                    <li>
+                        <Badge
+                            accent="green"
+                            href="https://mi.wycey.dev"
+                            aria-label="Go to Misskey managed by Wycey"
+                            target="_blank"
+                        >
+                            <Badge.Icon src="/icons/misskey.png" alt="Misskey" />
+                            Misskey
+                        </Badge>
+                    </li>
+                </NavigationList>
+            </NavigationArea>
+        </HeaderBase>
+    );
+});
---
+++ apps/web/src/routes/layout.tsx
@@ -0,0 +1,16 @@
+import { component$, Slot } from "@builder.io/qwik";
+
+import Footer from "@/components/footer";
+import Header from "@/components/header";
+
+export default component$(() => (
+    <>
+        <Header />
+        <main>
+            <section>
+                <Slot />
+            </section>
+        </main>
+        <Footer />
+    </>
+));
