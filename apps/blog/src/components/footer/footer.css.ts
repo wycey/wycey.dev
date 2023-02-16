cae6719834be15d2f6b4f53326c25ac6e506e400
Mido <instructr13@gmail.com>
2023-02-17 01:12:35 +0900

    chore(blog): let's tailwind

--- apps/blog/src/components/footer/footer.css.ts
+++ apps/blog/src/components/footer/footer.css.ts
@@ -4,16 +4,4 @@ export const FooterLink = styled.a`
     color: var(--primary);
 `;
 
-export const FooterBase = styled.footer`
-    display: grid;
-    grid-template-columns: auto 1fr auto;
-    align-content: center;
-
-    width: 100vw;
-    margin: 0;
-    margin-top: 3rem;
-    padding-inline: 3rem;
-    padding-bottom: 1rem;
-`;
-
 export default "";
