294f1be2eadd495e854aaed07763b4d9186319c8
Mido <instructr13@gmail.com>
2023-02-17 01:12:35 +0900

    chore(blog): let's tailwind

--- apps/blog/src/components/footer/index.tsx
+++ apps/blog/src/components/footer/index.tsx
@@ -1,12 +1,12 @@
 import { component$, useStyles$ } from "@builder.io/qwik";
 
-import _styles, { FooterBase, FooterLink } from "./footer.css";
+import _styles, { FooterLink } from "./footer.css";
 
 const Footer = component$(() => {
     useStyles$(_styles);
 
     return (
-        <FooterBase>
+        <footer class="m-0 mt-12 grid w-screen grid-cols-[auto_1fr_auto] content-center px-12 pb-4">
             <p>&copy; 2023 Wycey</p>
             <div />
             <p>
@@ -19,7 +19,7 @@ const Footer = component$(() => {
                     Cloudflare Pages
                 </FooterLink>
             </p>
-        </FooterBase>
+        </footer>
     );
 });
 
