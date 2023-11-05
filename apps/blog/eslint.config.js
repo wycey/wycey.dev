d2bf0bf4a235bdea530b18dc8470ebbf95b020ad
Mido <mido@wycey.dev>
2023-11-05 23:33:25 +0900

    switch to bun & astro in blog

--- apps/blog/eslint.config.js
+++ apps/blog/eslint.config.js
@@ -1,7 +1,61 @@
+import { dirname } from "node:path";
+import { fileURLToPath } from "node:url";
+
+import { FlatCompat } from "@eslint/eslintrc";
+import unicorn from "eslint-plugin-unicorn";
+
+import baseConfig from "../../eslint.config.js";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
+
+const ignores = [".astro"];
+
 export default [
+  ...baseConfig,
+  {
+    ignores,
+  },
+  ...compat.extends("plugin:astro/recommended"),
   {
     rules: {
       "no-console": "warn",
+    },
+  },
+  {
+    files: ["src/pages/**/*.{j,t}s{,x}"],
+    plugins: {
+      unicorn,
+    },
+    rules: {
+      "unicorn/filename-case": [
+        "error",
+        {
+          case: "kebabCase",
+        },
+      ],
+    },
+  },
+  {
+    files: ["src/components/**/*.{j,t}s{,x}"],
+    plugins: {
+      unicorn,
+    },
+    rules: {
+      "unicorn/filename-case": [
+        "error",
+        {
+          cases: {
+            pascalCase: true,
+            kebabCase: true,
+            camelCase: true,
+          },
+        },
+      ],
     },
   },
 ];
