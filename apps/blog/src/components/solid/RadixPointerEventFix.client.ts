f146da74d2093dfc7703576ae2062d6063903996
Mido <mido@wycey.dev>
2026-03-17 03:20:49 +0900

    fix: simplify pointer event handling

--- apps/blog/src/components/solid/RadixPointerEventFix.client.ts
+++ apps/blog/src/components/solid/RadixPointerEventFix.client.ts
@@ -1,23 +1,14 @@
 // Workaround for https://github.com/radix-ui/primitives/issues/2580
 
-import { onCleanup, onMount } from "solid-js";
+import { createEventListener } from "@solid-primitives/event-listener";
+import { onMount } from "solid-js";
 import { isServer } from "solid-js/web";
 
 export const RadixPointerEventFix = () => {
-  const listener = () => {};
-
   onMount(() => {
     if (isServer || !window.document?.body) return;
 
-    window.document.body.addEventListener("pointerdown", listener, {
-      passive: true,
-    });
-  });
-
-  onCleanup(() => {
-    if (isServer || !window.document?.body) return;
-
-    window.document.body.removeEventListener("pointerdown", listener);
+    createEventListener(window.document.body, "pointerdown", () => {});
   });
 
   return null;
