fde9592ed4d505e7a04144400a95f0f0c5dfdaa0
Mido <mido@wycey.dev>
2026-03-20 02:05:16 +0900

    fix: use another approach to use sentry

--- apps/blog/astro.config.ts
+++ apps/blog/astro.config.ts
@@ -72,6 +72,9 @@ export default defineConfig({
       build: "compile",
       runtime: "cloudflare-binding",
     },
+    // https://annzeyvf.blogspot.com/?page=en-git-withastro-astro-1773552299403
+    // https://github.com/martinsilha/astro-cloudflare-sentry-prerender-bug
+    prerenderEnvironment: import.meta.env.PROD ? "node" : "workerd",
   }),
   trailingSlash: "never",
   build: {
--- apps/blog/sentry.prerender.config.ts
+++
@@ -1,2 +0,0 @@
-// https://annzeyvf.blogspot.com/?page=en-git-withastro-astro-1773552299403
-// https://github.com/martinsilha/astro-cloudflare-sentry-prerender-bug
